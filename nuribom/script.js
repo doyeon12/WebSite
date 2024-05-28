194171c446b342e43f15ef97009169911818fce0
doyeon12 <101300245+doyeon12@users.noreply.github.com>
2024-05-28 11:22:04 +0900

    Feat: JavaScript-login-succes

--- nuribom/script.js
+++ nuribom/script.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const loginButton = document.querySelector('#loginButton');
+    const loginButton = document.querySelector('.headerBtn button:nth-of-type(1)');
     const loginModal = document.querySelector('.logb');
     const closeButton = document.querySelector('.logb .close1');
-    const loginSubmitButton = document.querySelector('.logb .submit');
+    const loginSubmitButton = document.querySelector('.logb .push');
 
     // 로그인 버튼 클릭 시 로그인 모달 열기
     loginButton.addEventListener('click', function () {
@@ -16,21 +16,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // 로그인 제출 버튼 클릭 시 입력 값 확인
     loginSubmitButton.addEventListener('click', function () {
-        const inputs = loginModal.querySelectorAll('input');
-        let allFieldsFilled = true;
+        const idInput = loginModal.querySelector('input[type="text"]');
+        const passwordInput = loginModal.querySelector('input[type="password"]');
 
-        inputs.forEach(input => {
-            if (input.value.trim() === '') {
-                allFieldsFilled = false;
-            }
-        });
-
-        if (!allFieldsFilled) {
+        if (idInput.value.trim() === '' || passwordInput.value.trim() === '') {
             alert('다시 시도해주세요.');
         } else {
             alert('로그인 되었습니다!');
             // 입력 필드 값을 지우기
-            inputs.forEach(input => input.value = '');
+            idInput.value = '';
+            passwordInput.value = '';
             // 홈 페이지로 리다이렉트
             window.location.href = 'index.html'; // 홈 페이지의 URL을 적어주세요
         }
